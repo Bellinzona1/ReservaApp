83ac1b5f85073a523c93c68a07fae64b9bda7bde
MateoBellinzona <mtbellinzona@gmail.com>
2025-04-22 16:00:19 -0300

    code verifiquer activado

--- Server/src/controllers/MpConect.controller.js
+++ Server/src/controllers/MpConect.controller.js
@@ -2,47 +2,38 @@ const User = require("../models/user.model");
 const axios = require("axios");
 
 
+
 const conectarMercadoPago = async (req, res) => {
-  const { code } = req.body;
-
-  console.log("Código recibido:", code); // Agregado para depuración
-
-  if (!code) {
-    return res.status(400).json({ message: "Código de autorización faltante" });
-  }
-
-  try {
-    const response = await axios.post("https://api.mercadopago.com/oauth/token", {
-      grant_type: "authorization_code",
-      client_id: "4385724245174951",
-      client_secret: 'cDZnord5P8C7CVopIqJXnt28j96LXgVO',
-      code,
-      redirect_uri: "https://genuine-elf-ca9b01.netlify.app/conectmp"
-    });
-
-    const { access_token, user_id } = response.data;
-
-    // Buscar el usuario autenticado y actualizar sus datos de Mercado Pago
-    const user = await User.findByIdAndUpdate(
-      req.userId,
-      {
-        mercadoPagoToken: access_token,
-        mercadoPagoUserId: user_id,
-      },
-      { new: true }
-    );
-
-    if (!user) {
-      return res.status(404).json({ message: "Usuario no encontrado" });
+    const { code, code_verifier } = req.body;
+  
+    if (!code || !code_verifier) {
+      return res.status(400).json({ message: "Faltan parámetros" });
     }
-
-    res.json({ message: "Cuenta de Mercado Pago vinculada con éxito", user });
-  } catch (error) {
-    console.log(error)
-    console.error("Error al conectar con Mercado Pago:", error.response?.data || error.message);
-    res.status(500).json({ message: "Error al conectar con Mercado Pago" });
-  }
-};
+  
+    try {
+      const response = await axios.post("https://api.mercadopago.com/oauth/token", {
+        grant_type: "authorization_code",
+        client_id: "4385724245174951",
+        code,
+        code_verifier,
+        redirect_uri: "https://genuine-elf-ca9b01.netlify.app/conectmp"
+      });
+  
+      const { access_token, user_id } = response.data;
+  
+      const user = await User.findByIdAndUpdate(
+        req.userId,
+        { mercadoPagoToken: access_token, mercadoPagoUserId: user_id },
+        { new: true }
+      );
+  
+      res.json({ message: "Vinculación exitosa", user });
+    } catch (error) {
+      console.error("Error:", error.response?.data || error.message);
+      res.status(500).json({ message: "Error al conectar con Mercado Pago", detalle: error.response?.data });
+    }
+  };
+  
 
 module.exports = {
   conectarMercadoPago,
